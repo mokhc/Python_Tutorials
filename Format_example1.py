d490ad6802974b201dec795c2175178ec25c2574
mokhc <79033377+mokhc@users.noreply.github.com>
2023-03-01 17:32:41 +0800

    Add files via upload

---
+++ Format_example1.py
@@ -0,0 +1,16 @@
+# Test Program
+# Format output - example 1
+# Trivial - What is need to print a variable within an output string?
+
+# To get the input
+number = input('Enter a number: ')
+number = float(number)
+
+# Assignment
+sqrtofnum = number ** 0.5
+
+# output format 1
+print('The square root of %0.3f is %0.3f'%(number, sqrtofnum))
+
+# output format 2
+print("The square root of {0:0.3f} is {1:0.3f}".format(number, sqrtofnum))
