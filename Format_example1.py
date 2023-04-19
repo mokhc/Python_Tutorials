3a6e2e2632e1ed821c79c7bde3770b45d1f341df
mokhc <79033377+mokhc@users.noreply.github.com>
2023-04-20 01:39:06 +0800

    Update Format_example1.py

--- Format_example1.py
+++ Format_example1.py
@@ -10,7 +10,7 @@ number = float(number)
 sqrtofnum = number ** 0.5
 
 # output format 1
-print('The square root of %0.3f is %0.3f'%(number, sqrtofnum))
+print('The square root of %0.3f is %0.3f' % (number, sqrtofnum)) # whitespace around the % operator
 
 # output format 2
 print("The square root of {0:0.3f} is {1:0.3f}".format(number, sqrtofnum))
