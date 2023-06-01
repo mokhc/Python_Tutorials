d2f5075f320fe24b77ac8d0c27eb4eaa161e1545
mokhc <79033377+mokhc@users.noreply.github.com>
2023-06-01 21:46:54 +0800

    Add files via upload

---
+++ Lambda_example1.py
@@ -0,0 +1,15 @@
+# Test Program - Lambda functions
+# Trivial - What is needed in python when you declare a lambda function?
+
+# lambda function
+number1 = lambda num1: print(num1)
+
+# lambda function that returns a value
+number2 = lambda num2: num2
+
+
+# Invoke the lambda functions
+number1(5)
+
+number3 = number2(4)
+print(number3)
