f60b6c60cce8be041af5a163298698d8a0e4e9b4
mokhc <79033377+mokhc@users.noreply.github.com>
2023-06-01 22:19:56 +0800

    Add files via upload

--- Lambda_example1.py
+++ Lambda_example1.py
@@ -2,23 +2,50 @@
 # Trivial - What is needed in python when you declare a lambda function?
 
 # lambda functions
-number1 = lambda num1: print(num1)
+number1a = lambda num1: print(num1)
 
-number2 = lambda num1, num2: print(num1 + num2)
+number2a = lambda num1, num2: print(num1 + num2)
+
+
+# equivalence
+def number1b(num1):
+    print(num1)
+
+
+def number2b(num1, num2):
+    print(num1 + num2)
+
 
 # lambda functions that return a value
-number3 = lambda num1, num2: num1 + num2
+number3a = lambda num1, num2: num1 + num2
 
-number4 = lambda num1 : num1
+number4a = lambda num1 : num1
+
+
+# equivalence
+def number3b(num1, num2):
+    return num1 + num2
+
+
+def number4b(num1):
+    return num1
 
 
 # Invoke the lambda functions
-number1(5)
+number1a(5)
+number1b(5)
 
-number2(5, 0)
+number2a(6, 0)
+number2b(6, 0)
 
-number3 = number3(4, 0)
-print(number3)
+number3a = number3a(7, 0)
+print(number3a)
 
-number4 = number4(4)
-print(number4)
+number3b = number3b(7, 0)
+print(number3b)
+
+number4a = number4a(8)
+print(number4a)
+
+number4b = number4b(8)
+print(number4b)
